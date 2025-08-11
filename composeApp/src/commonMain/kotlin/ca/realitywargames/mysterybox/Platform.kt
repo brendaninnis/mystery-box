76b3a3648f0ee9beea7e3e150e611938ec9d78bb
Brendan Innis <innisbrendan@gmail.com>
2025-08-11 08:40:04 -0700

    Simplify template

--- composeApp/src/commonMain/kotlin/ca/realitywargames/mysterybox/Platform.kt
+++
@@ -1,7 +0,0 @@
-package ca.realitywargames.mysterybox
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
--- composeApp/src/iosMain/kotlin/ca/realitywargames/mysterybox/Platform.ios.kt
+++
@@ -1,9 +0,0 @@
-package ca.realitywargames.mysterybox
-
-import platform.UIKit.UIDevice
-
-class IOSPlatform: Platform {
-    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
-}
-
-actual fun getPlatform(): Platform = IOSPlatform()
